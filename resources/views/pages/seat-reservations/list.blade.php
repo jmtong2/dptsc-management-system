c11c5722babb985365264a196d2f55fe6feb0464
jmtong2 <jmtong2@yahoo.com>
2023-07-17 01:55:05 +0800

    deleted add seat reservation

--- resources/views/pages/seat-reservations/list.blade.php
+++ resources/views/pages/seat-reservations/list.blade.php
@@ -7,8 +7,8 @@
                         <h4 class="mb-3">Seat Reservations List</h4>
                         {{--                        <p class="mb-0">The product list effectively dictates product presentation and provides space<br> to list your products and offering in the most appealing way.</p>--}}
                     </div>
-                    <a href="{{route('seat-reservations.create')}}" class="btn btn-primary add-list"><i
-                            class="las la-plus mr-3"></i>Add Seat Reservation</a>
+                    <!-- <a href="{{route('seat-reservations.create')}}" class="btn btn-primary add-list"><i
+                            class="las la-plus mr-3"></i>Add Seat Reservation</a> -->
                 </div>
             </div>
             <div class="col-lg-12">
